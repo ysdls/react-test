59ddde731b7598d7f8b30faf6f018ceb1f0923b5
ysdls <ysdls84@gmail.com>
2023-10-18 13:14:22 +0900

    test

--- src/App.js
+++ src/App.js
@@ -5,29 +5,24 @@ import { useRef, useEffect, useState } from "react";
 import Test from "./Test";
 
 function App() {
-    return (
-        <>
-            <App />
-        </>
-    );
-    //return
-    //   <div className="App">
-    //     <header className="App-header">
-    //       <img src={logo} className="App-logo" alt="logo" />
-    //       <p>
-    //         Edit <code>src/App.js</code> and save to reload.
-    //       </p>
-    //       <a
-    //         className="App-link"
-    //         href="https://reactjs.org"
-    //         target="_blank"
-    //         rel="noopener noreferrer"
-    //       >
-    //         Learn React
-    //       </a>
-    //     </header>
-    //   </div>
+    // return (
+    //     <>
+    //         <App />
+    //     </>
     // );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                    Learn React
+                </a>
+            </header>
+        </div>
+    );
 }
 
 export default App;
